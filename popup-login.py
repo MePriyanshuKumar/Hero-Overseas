3d0eb19ed6b05e203219ddd1c4f7c5e3e39adafd
Priyanshu <99090719+MePriyanshuKumar@users.noreply.github.com>
2022-09-17 15:28:28 +0530

    Now to work on functionality and database

--- popup-login.py
+++ popup-login.py
@@ -2,6 +2,7 @@
 from tkinter import *
 from tkinter import ttk
 from turtle import bgcolor
+import os # for linking the file
 #Create an instance of Tkinter frame
 win = Tk()
 #Set the geometry of Tkinter frame
@@ -9,10 +10,13 @@ win.geometry("400x200")
 win.configure(bg='#A52A2A') 
 win.title("Hero Overseas")
 def open_popup():
-   top= Toplevel(win)
-   top.geometry("750x250")
-   top.title("Child Window")
-   Label(top, text= "Hello World!", font=('Mistral 18 bold')).place(x=150,y=80)
+    filename= 'login.py'
+    os.system(filename) #Open file [same as Right Click open]
+    os.system('tkinter'+ filename)
+#    top= Toplevel(win)
+#    top.geometry("750x250")
+#    top.title("Child Window")
+#    Label(top, text= "Welcome to Hero Overseas!", font=('Mistral 18 bold')).place(x=150,y=80)
 
 Label(win, bg="#A52A2A",text=" Please login to take the test!", font=('Anton')).pack(pady=20)
 #Create a button in the main Window to open the popup
