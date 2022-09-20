1d72775a7c720560a1d9af1362395957a34a81c6
Priyanshu <99090719+MePriyanshuKumar@users.noreply.github.com>
2022-09-20 21:44:02 +0530

    Connected to mysql  database

--- register.py
+++ register.py
@@ -5,13 +5,22 @@ from tkinter import ttk
 from tkinter import messagebox
 
 from PIL import Image, ImageTk
-
+import mysql.connector
 
 class Register:
     def __init__(self, root):
         self.root = root
         self.root.title("Register")
         self.root.geometry("1550x800+0+0")
+
+        # == =text variables
+        self.var_fname = StringVar()
+        self.var_lname = StringVar()
+        self.var_contact = StringVar()
+        self.var_email = StringVar()
+        self.var_pass = StringVar()
+        self.var_confpass = StringVar()
+     
 
         # ====bg image====
         self.bg = ImageTk.PhotoImage(
@@ -35,70 +44,100 @@ class Register:
 
         # ====row 1
         # labels and entry fields
-        fname = Label(frame, text="First Name", font=(
-            "times new roman", 15, "bold"), bg="white")
+        fname = Label(frame, text="First Name", font=("times new roman", 15, "bold"), bg="white")
         fname.place(x=50, y=100)
 
-        fname_entry = ttk.Entry(frame, font=("times new roman", 15, "bold"))
-        fname_entry.place(x=50, y=130, width=250)
+        self.fname_entry = ttk.Entry(frame, textvariable=self.var_fname ,font=("times new roman", 15, "bold"))
+        self.fname_entry.place(x=50, y=130, width=250)
 
-        l_name = Label(frame, text = "Last Name", font =("times new roman", 15, "bold"), bg = "white", fg = "black")
-        l_name.place(x = 370, y = 100)
+        l_name = Label(frame, text="Last Name", font=(
+            "times new roman", 15, "bold"), bg="white", fg="black")
+        l_name.place(x=370, y=100)
 
-        self.txt_lname = ttk.Entry(frame, font= ("times new roman", 15))
-        self.txt_lname.place(x = 370, y= 130, width = 250)
+        self.txt_lname = ttk.Entry(frame, textvariable=self.var_lname,font=("times new roman", 15))
+        self.txt_lname.place(x=370, y=130, width=250)
 
         # == row 2
         # contact us
-        contact = Label(frame, text = "Mobile No", font=("times new roman",15, "bold"), bg = "white", fg="black")
-        contact.place (x=50, y = 170)
+        contact = Label(frame, text="Mobile No", font=(
+            "times new roman", 15, "bold"), bg="white", fg="black")
+        contact.place(x=50, y=170)
 
-        self.txt_contact = ttk.Entry(frame, font= ("times new roman", 15))
-        self.txt_contact.place(x = 50, y= 200, width = 250)
+        self.txt_contact = ttk.Entry(frame,textvariable=self.var_contact, font=("times new roman", 15))
+        self.txt_contact.place(x=50, y=200, width=250)
 
         # email address
-        email = Label(frame, text = "Email", font = ("times new roman", 15, "bold"), bg= "white", fg = "black")
-        email.place(x = 370, y = 170)
+        email = Label(frame, text="Email", font=(
+            "times new roman", 15, "bold"), bg="white", fg="black")
+        email.place(x=370, y=170)
 
-        
-        self.txt_email = ttk.Entry(frame, font= ("times new roman", 15))
-        self.txt_email.place(x = 370, y= 200, width = 250)
+        self.txt_email = ttk.Entry(frame,textvariable=self.var_email, font=("times new roman", 15))
+        self.txt_email.place(x=370, y=200, width=250)
 
         # row 3
-        pswd = Label(frame, text ="Password", font= ("times new roman",15, "bold"),bg="white", fg="black")
-        pswd.place(x = 50, y = 240)
+        pswd = Label(frame, text="Password", font=(
+            "times new roman", 15, "bold"), bg="white", fg="black")
+        pswd.place(x=50, y=240)
 
-        self.txt_pswd = ttk.Entry(frame, font=("times new roman",15))
-        self.txt_pswd.place(x =50, y = 270, width = 250)
+        self.txt_pswd = ttk.Entry(frame,textvariable=self.var_pass, font=("times new roman", 15))
+        self.txt_pswd.place(x=50, y=270, width=250)
 
-        confirm_pswd = Label(frame, text =" Confirm Password", font= ("times new roman",15, "bold"),bg="white", fg="black")
-        confirm_pswd.place(x = 370, y = 240)
+        confirm_pswd = Label(frame, text=" Confirm Password", font=(
+            "times new roman", 15, "bold"), bg="white", fg="black")
+        confirm_pswd.place(x=370, y=240)
 
-        self.txt_confirm_pswd = ttk.Entry(frame, font=("times new roman",15))
-        self.txt_confirm_pswd.place(x =370, y = 270, width = 250)
+        self.txt_confirm_pswd = ttk.Entry(frame,textvariable=self.var_confpass, font=("times new roman", 15))
+        self.txt_confirm_pswd.place(x=370, y=270, width=250)
 
         # checkbox=====
-        checkbtn = Checkbutton(frame, text = "I Agree the Terms & Conditions",font=("italic",12,"bold"),onvalue=1,offvalue=0)
-        checkbtn.place(x= 50, y =330)
+        self.var_check = IntVar()
+        checkbtn = Checkbutton(frame,variable=self.var_check, text="I Agree the Terms & Conditions", font=(
+            "italic", 12, "bold"), onvalue=1, offvalue=0)
+        checkbtn.place(x=50, y=330)
 
-        #button register now
+        # button register now
         img = Image.open(r"D:\Projects\Hero Overseas\register-now-btn.jpeg")
-        img = img.resize((150,100),Image.ANTIALIAS) 
+        img = img.resize((150, 100), Image.ANTIALIAS)
         self.photoimage = ImageTk.PhotoImage(img)
-        b1 = Button(frame, image = self.photoimage, borderwidth=0,cursor="hand2",font = ("times new roman",15, "bold"),bg="white",fg="black")
-        b1.place(x = 10, y = 380, width=200)
+        b1 = Button(frame, image=self.photoimage,command = self.register_data, borderwidth=0, cursor="hand2", font=(
+            "times new roman", 15, "bold"), bg="white", fg="black")
+        b1.place(x=10, y=380, width=200)
 
-        #login btn
+        # login btn
         img1 = Image.open(r"D:\Projects\Hero Overseas\login-now-btn.jpg")
-        img1 = img1.resize((150,100),Image.ANTIALIAS) 
+        img1 = img1.resize((150, 100), Image.ANTIALIAS)
         self.photoimage1 = ImageTk.PhotoImage(img1)
-        b1 = Button(frame, image = self.photoimage1, borderwidth=0,cursor="hand2",font = ("times new roman",15, "bold"),bg="white",fg="black")
-        b1.place(x = 330, y = 380, width=200)
+        b1 = Button(frame, image=self.photoimage1, borderwidth=0, cursor="hand2", font=(
+            "times new roman", 15, "bold"), bg="white", fg="black")
+        b1.place(x=330, y=380, width=200)
 
+        # ===== Function declarations =====
 
-
-
-
+    def register_data(self):
+        if self.var_fname.get() == "" or self.var_contact.get() == "" or self.var_email.get() == "" :
+            messagebox.showerror("Error","All fields are required")
+        elif self.var_pass.get() != self.var_confpass.get():
+            messagebox.showerror("Error","password & confirm password must be same")
+        elif self.var_check.get() == 0:
+            messagebox.showerror("Error","Please agree our terms and conditions")
+        else:
+            conn = mysql.connector.connect(host = "localhost", user = "root", password = "root@123", database = "herooverseas")
+            # add
+            my_cursor = conn.cursor()
+            # validation
+            query = ("SELECT * FROM register WHERE email = %s")
+            value = (self.var_email.get(),)
+            my_cursor.execute(query, value)
+            #fetch data from database
+            row = my_cursor.fetchone()
+            if row != None:
+                messagebox.showerror("Error","User already exist , please try signing in")
+            else:
+                #data enter
+                my_cursor.execute("insert into register values(%s,%s,%s,%s,%s)",(self.var_fname.get(),self.var_lname.get(),self.var_contact.get(),self.var_email.get(),self.var_pass.get()))    
+                conn.commit()
+                conn.close()
+                messagebox.showinfo("Success!", "Register successfully")
 
 
 if __name__ == "__main__":
