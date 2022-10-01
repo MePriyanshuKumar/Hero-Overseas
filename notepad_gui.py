ed9d4800ea60a1a634f6a9eebcdf20b344fa4ebf
Priyanshu <99090719+MePriyanshuKumar@users.noreply.github.com>
2022-10-01 21:48:35 +0530

    Created notepad for heroOverseas

--- notepad_gui.py
+++ notepad_gui.py
@@ -1,88 +1,112 @@
-#starting complete notepad GUI for Hero Overseas
-
-from copy import copy
+#  starting Notepad GUI
 from tkinter import *
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os
 
-from click import command
-class notepad:
-    
-
-    # Function declaration
-    def newFile():
-        pass
-
-    def openFile():
-        pass
-
-    def saveFile():
-        pass
-
-    def quitApp():
-        pass
-
-    def cut():
-        pass
-
-    def copy():
-        pass
-
-    def paste():
-        pass
-
-    def about():
-        pass
-
-    if __name__ == '__main__':
-          #Basic setup
-        root = Tk()
-        root.title("Untitled - Notepad")
-
-        root.wm_iconbitmap("icons8-notepad.ico")
-
-        root.geometry("640x788")
-        # add textarea
-        TextArea = Text(root, font="lucida 13")
-        file = None
-        TextArea.pack(fill = BOTH)
-        
-        #Menubar option
-        MenuBar = Menu(root)
-        #Filemenu starts
-        FileMenu = Menu(MenuBar, tearoff=0)
+def newFile():
+    global file
+    root.title("Untitled - Notepad")
+    file = None
+    TextArea.delete(1.0, END)
 
 
-    #To open a new file
-        FileMenu.add_command(label="New", command=newFile)
-
-        #To open already existing file
-        FileMenu.add_command(label="Open", command=openFile)
-        
-        #To save current file
-        FileMenu.add_command(label="Save", command=saveFile)
-
-        FileMenu.add_separator()
-        FileMenu.add_command(label= "Exit", command = quitApp)
-        MenuBar.add_cascade(label = "File", menu=FileMenu)
-        #Filemenu ends
-
-        #Edit menu starts
-        EditMenu = Menu(MenuBar,tearoff=0)
-        # To give feature of cut, copy and paste
-        EditMenu.add_command(label = "Cut", command=cut)
-        EditMenu.add_command(label = "Copy", command=copy)
-        EditMenu.add_command(label = "Paste", command=paste)
-
-        MenuBar.add_cascade(label = "Edit", menu = EditMenu)
-        # Edit menu ends
-
-        # Help menu starts
-        HelpMenu = Menu(MenuBar, tearoff=0)
-        HelpMenu.add_command(label = "About Notepad", command = about)
-        MenuBar.add_cascade(label="Help", menu= HelpMenu)
-        # Help menu ends
 
 
-        root.config(menu=MenuBar)
+
+def saveFile():
+    global file
+    if file == None:
+        file = asksaveasfilename(initialfile = 'Untitled.txt', defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+        if file =="":
+            file = None
+
+        else:
+            #Save as a new file
+            f = open(file, "w")
+            f.write(TextArea.get(1.0, END))
+            f.close()
+
+            root.title(os.path.basename(file) + " - Notepad")
+            print("File Saved")
+    else:
+        # Save the file
+        f = open(file, "w")
+        f.write(TextArea.get(1.0, END))
+        f.close()
 
 
-        root.mainloop()
+def quitApp():
+    root.destroy()
+
+def cut():
+    TextArea.event_generate(("<>"))
+
+def copy():
+    TextArea.event_generate(("<>"))
+
+def paste():
+    TextArea.event_generate(("<>"))
+
+def about():
+    showinfo("Notepad", "Notepad for writing notes while IELTS-PTE Test. Hero Overseas has always been committed to words given to our students and clients for the best experience in terms of VISA consultancy & IELTS, PTE etc. training…")
+
+if __name__ == '__main__':
+    #Basic tkinter setup
+    root = Tk()
+    root.title("Untitled - Notepad")
+    root.wm_iconbitmap("icons8-notepad.ico")
+    root.geometry("644x788")
+
+    #Add TextArea
+    TextArea = Text(root, font="lucida 13")
+    file = None
+    TextArea.pack(expand=True, fill=BOTH)
+
+    # Lets create a menubar
+    MenuBar = Menu(root)
+
+    #File Menu Starts
+    FileMenu = Menu(MenuBar, tearoff=0)
+    # To open new file
+    FileMenu.add_command(label="New", command=newFile)
+
+  
+
+    # To save the current file
+
+    FileMenu.add_command(label = "Save", command = saveFile)
+    FileMenu.add_separator()
+    FileMenu.add_command(label = "Exit", command = quitApp)
+    MenuBar.add_cascade(label = "File", menu=FileMenu)
+    # File Menu ends
+
+    # Edit Menu Starts
+    EditMenu = Menu(MenuBar, tearoff=0)
+    #To give a feature of cut, copy and paste
+    EditMenu.add_command(label = "Cut", command=cut)
+    EditMenu.add_command(label = "Copy", command=copy)
+    EditMenu.add_command(label = "Paste", command=paste)
+
+    MenuBar.add_cascade(label="Edit", menu = EditMenu)
+
+    # Edit Menu Ends
+
+    # Help Menu Starts
+    HelpMenu = Menu(MenuBar, tearoff=0)
+    HelpMenu.add_command(label = "About Notepad", command=about)
+    MenuBar.add_cascade(label="Help", menu=HelpMenu)
+
+    # Help Menu Ends
+
+    root.config(menu=MenuBar)
+
+    #Adding Scrollbar 
+    Scroll = Scrollbar(TextArea)
+    Scroll.pack(side=RIGHT,  fill=Y)
+    Scroll.config(command=TextArea.yview)
+    TextArea.config(yscrollcommand=Scroll.set)
+
+    root.mainloop()
